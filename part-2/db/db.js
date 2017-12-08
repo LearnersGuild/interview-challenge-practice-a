f6ce4f20ebf7a4c12ee7a102be8e739106de7c93
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-08 14:31:03 -0800

    Update db.js in parts 2 and 3 to use new .env config that contains only DATABASE_URL

--- part-2/db/db.js
+++ part-2/db/db.js
@@ -1,18 +1,17 @@
-const path = require('path')
-require('dotenv').config({path: path.join(__dirname, '../../.env')})
+require('dotenv').config({path: require('path').join(__dirname, '../.env')})
+
+const url = require('url')
 const pgp = require('pg-promise')()
 
-const connection = {
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: true
-}
+const {hostname: host, port, pathname, auth} = url.parse(process.env.DATABASE_URL)
+const database = pathname ? pathname.slice(1) : undefined
+const user = auth ? auth.split(':')[0] : undefined
+const password = auth ? auth.split(':')[1] : undefined
+const connection = {host, port, database, user, password, ssl: true}
 
 const db = pgp(connection)
 db.connect()
+
 
 /**
  * Get the names for passengers on a specific flight

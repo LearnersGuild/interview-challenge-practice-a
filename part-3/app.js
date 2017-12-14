a96e9b42e06c8db154d259544a533e03dd08686d
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-14 10:48:25 -0800

    Update flight_passengers to flight_roster in the app for part 3

--- part-3/app.js
+++ part-3/app.js
@@ -7,12 +7,16 @@ app.use(express.static('public'))
 // app.set('view engine', 'pug')
 // app.set('view engine', 'ejs')
 
-app.get('/flight_passengers', (req, res) => {
-  const flightNumber = req.query.flightNumber
+app.get('/flight_roster', (req, res) => {
+  const flightNumber = req.query.flightNumber;
   getFlightPassengers(flightNumber)
-    .then((passengers) => res.render('flight_passengers', {flightNumber, passengers}))
-    .catch((err) => res.render('flight_passengers', {message: `An error occurred: ${err.toString()}`}))
-    .catch(console.error)
+    .then((passengers) => {
+      res.render('flight_roster', { flightNumber, passengers });
+    })
+    .catch((err) => {
+      res.render('flight_roster', { message: `An error occurred: ${err.toString()}` });
+    })
+    .catch(console.error);
 });
 
 app.listen(3000, () => console.log('Flying high on port 3000!'))
